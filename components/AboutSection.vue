0c7a8d8adeac21d461209e9978185847b41d5672
AlvigoBuana <www.alvigowahyubuana@gmail.com>
2024-06-27 16:55:06 +0700

    responsi

---
+++ components/AboutSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="about">
+    <h2>About Me</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutSection'
+}
+</script>
+
+<style scoped>
+.about {
+  padding: 50px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HeroSection />
+  </div>
+  <div>
+    <AboutSection />
+  </div>
+</template>
+
+<script>
+import HeroSection from '~/components/HeroSection.vue';
+import AboutSection from '~/components/AboutSection.vue';
+import ProjectsSection from '~/components/ProjectsSection.vue';
+import ContactSection from '~/components/ContactSection.vue';
+
+export default {
+  components: {
+    HeroSection,
+    AboutSection,
+    ContactSection,
+    ProjectsSection
+  }
+}
+</script>./Index.vue
